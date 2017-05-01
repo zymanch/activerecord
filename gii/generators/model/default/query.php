94821e405e620cb6c7a5b91858174f50d64597e8
ZyManch <zymanch@gmail.com>
2017-05-01 18:45:25 +0400

    [refs #16665] fix bug with filter method4

--- gii/generators/model/default/query.php
+++ gii/generators/model/default/query.php
@@ -23,7 +23,7 @@ namespace <?= $ns.'\\'.$sub ?>;
 /**
  * This is the ActiveQuery class for [[<?= $ns.'\\'.$mainClassName ?>]].
 <?php foreach ($tableSchema->columns as $column): ?>
- * @method <?= $ns.'\\'.$mainQueryClassName ?> filterBy<?= str_replace('_', '', ucwords($column->name, '_')); ?>($value, $criteria = null)
+ * @method \<?= $ns.'\\'.$mainQueryClassName ?> filterBy<?= str_replace('_', '', ucwords($column->name, '_')); ?>($value, $criteria = null)
 <?php endforeach; ?>
  */
 class <?= $queryClassName ?> extends <?= '\\' . ltrim($generator->queryBaseClass, '\\') . "\n" ?>
